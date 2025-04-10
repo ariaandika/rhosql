3bc9fda15247c07872f73edc5961d079769ff630
ariaandika <andikaakun17@gmail.com>
2025-04-10 16:54:12 +0700

    use arc instead of manual atomic usize

--- src/sqlite/database.rs
+++ src/sqlite/database.rs
@@ -1,5 +1,5 @@
 use libsqlite3_sys::{self as ffi};
-use std::{ffi::CStr, ptr, sync::atomic::{AtomicUsize, Ordering}, time::Duration};
+use std::{ffi::CStr, ptr, sync::Arc, time::Duration};
 
 use super::{OpenFlag, SqliteMutexGuard, StatementHandle};
 use crate::{
@@ -12,23 +12,19 @@ use crate::{
 /// it is required that sqlite compiled with serialized mode enabled, thus this type is `Send` and `Sync`
 ///
 /// this type is `Send` and `Sync`
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct SqliteHandle {
-    counter: *mut AtomicUsize,
-    sqlite: *mut ffi::sqlite3,
+    sqlite: Arc<*mut ffi::sqlite3>,
 }
 
-macro_rules! doc {
-    ($($tt:item)*) => { $(
-        #[doc = "SAFETY: Checked that sqlite compiled with SERIALIZE_MODE"]
-        #[doc = "thus synchronization is handled by sqlite"]
-        $tt
-    )* }
-}
-doc! {
-    unsafe impl Send for SqliteHandle { }
-    unsafe impl Sync for SqliteHandle { }
-}
+/// SAFETY: Checked that sqlite compiled with `SERIALIZE_MODE`
+/// thus synchronization is handled by sqlite
+unsafe impl Send for SqliteHandle{}
+
+/// SAFETY: Checked that sqlite compiled with `SERIALIZE_MODE`
+/// thus synchronization is handled by sqlite
+unsafe impl Sync for SqliteHandle{}
+
 
 impl SqliteHandle {
     /// open a sqlite database
@@ -59,14 +55,13 @@ impl SqliteHandle {
             )))?;
         }
 
-        let counter = Box::new(AtomicUsize::new(1));
-        let db = Self { counter: Box::leak(counter), sqlite };
+        let db = Self { sqlite: Arc::new(sqlite) };
         db.try_result::<OpenError>(result)?;
 
         // for unsafe `Send` and `Sync` impl
         // If the threading mode is Single-thread or Multi-thread then this routine returns a NULL pointer.
         // https://sqlite.org/c3ref/db_mutex.html
-        let mutex = unsafe { ffi::sqlite3_db_mutex(db.sqlite) };
+        let mutex = unsafe { ffi::sqlite3_db_mutex(*db.sqlite) };
         if mutex.is_null() {
             Err(OpenError::NotSerializeMode)?;
         }
@@ -113,7 +108,7 @@ impl SqliteHandle {
         let mut ppstmt = ptr::null_mut();
         let (ptr, len, _) = sql.as_nulstr();
         self.try_result::<PrepareError>(unsafe {
-            ffi::sqlite3_prepare_v2(self.sqlite, ptr, len, &mut ppstmt, ptr::null_mut())
+            ffi::sqlite3_prepare_v2(*self.sqlite, ptr, len, &mut ppstmt, ptr::null_mut())
         })?;
         debug_assert!(!ppstmt.is_null(), "we check result above");
         Ok(StatementHandle::new(ppstmt, self.clone()))
@@ -127,7 +122,7 @@ impl SqliteHandle {
     ///
     /// this is a wrapper for `sqlite3_last_insert_rowid()`
     pub fn last_insert_rowid(&self) -> i64 {
-        unsafe { ffi::sqlite3_last_insert_rowid(self.sqlite) }
+        unsafe { ffi::sqlite3_last_insert_rowid(*self.sqlite) }
     }
 
     /// attempt to enter a mutex, if another thread is already within the mutex,
@@ -136,7 +131,7 @@ impl SqliteHandle {
     /// this is a wrapper for `sqlite3_last_insert_rowid()`
     pub fn mutex_enter(&self) -> SqliteMutexGuard<'_> {
         let lock = unsafe {
-            let lock = ffi::sqlite3_db_mutex(self.sqlite);
+            let lock = ffi::sqlite3_db_mutex(*self.sqlite);
             assert!(!lock.is_null(),"connection guarantee to be in serialize mode");
             ffi::sqlite3_mutex_enter(lock);
             lock
@@ -161,7 +156,7 @@ impl SqliteHandle {
     /// this is a wrapper for `sqlite3_busy_timeout()`
     pub fn busy_timeout(&mut self, timeout: impl Into<Duration>) -> Result<(), ConfigureError> {
         let timeout = timeout.into().as_millis().try_into().unwrap_or(i32::MAX);
-        self.try_result(unsafe { ffi::sqlite3_busy_timeout(self.sqlite, timeout) })
+        self.try_result(unsafe { ffi::sqlite3_busy_timeout(*self.sqlite, timeout) })
     }
 
     /// enables or disables the extended result codes feature of SQLite.
@@ -170,7 +165,7 @@ impl SqliteHandle {
     ///
     /// this is a wrapper for `sqlite3_extended_result_codes()`
     pub fn extended_result_codes(&mut self, onoff: bool) -> Result<(), ConfigureError> {
-        self.try_result(unsafe { ffi::sqlite3_extended_result_codes(self.sqlite, onoff as _)})
+        self.try_result(unsafe { ffi::sqlite3_extended_result_codes(*self.sqlite, onoff as _)})
     }
 }
 
@@ -181,7 +176,7 @@ impl SqliteHandle {
     ///
     /// this is a wrapper for `sqlite3_errmsg()`
     pub fn errmsg(&self) -> Option<&str> {
-        let data = unsafe { ffi::sqlite3_errmsg(self.sqlite) };
+        let data = unsafe { ffi::sqlite3_errmsg(*self.sqlite) };
         if data.is_null() {
             return None;
         }
@@ -210,30 +205,16 @@ impl SqliteHandle {
     ///
     /// this is a wrapper for `sqlite3_extended_errcode()`
     pub fn extended_errcode(&self) -> i32 {
-        unsafe { ffi::sqlite3_extended_errcode(self.sqlite) }
-    }
-
-}
-
-impl Clone for SqliteHandle {
-    fn clone(&self) -> Self {
-        let counter = unsafe { (*self.counter).fetch_add(1, Ordering::Relaxed) };
-        if counter >= isize::MAX as _ {
-            std::process::abort()
-        }
-        Self {
-            counter: self.counter,
-            sqlite: self.sqlite,
-        }
+        unsafe { ffi::sqlite3_extended_errcode(*self.sqlite) }
     }
 }
 
 impl Drop for SqliteHandle {
     fn drop(&mut self) {
-        if unsafe { &mut *self.counter }.fetch_sub(1, Ordering::Release) != 1 {
+        if Arc::strong_count(&self.sqlite) != 1 {
             return
         }
-        unsafe { ffi::sqlite3_close(self.sqlite) };
+        unsafe { ffi::sqlite3_close(*self.sqlite); };
     }
 }
 
